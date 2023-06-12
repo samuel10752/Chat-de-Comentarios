c96cff9ee3adf8e56fa1b6d53ae757914fd24cc7
samuel107152 <samuelcauabarbosa114@gmail.com>
2023-06-11 21:43:38 -0300

    att de responsividade

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
   <meta charset="UTF-8">
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="lib/css/css.css">
   <link rel="stylesheet" href="lib/css/responsivel.css">
-  <title>Document</title>
+  <title>Chat de Coemntarios</title>
 </head>
 
 <body>
@@ -16,6 +16,7 @@
     <div class="like-icon">
      <img src="icons/opiniao.png" alt="">
     </div>
+
 
 
     <form class="form" action="add_comment.php" method="post">
